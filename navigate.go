2352e550ba9e3e2cb49c0dccb2cc82f94f1a9a34
Peter Bom Jakobsen <pejak1990@gmail.com>
2017-04-26 15:05:53 +0200

    More ways to detect drone possition.

--- navigate.go
+++ navigate.go
@@ -1,8 +1,35 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"math"
+)
 
+const (
+	VIRTICAL   = iota
+	HORIZONTAL = iota
+	STAY       = iota
+)
+
+// ErrDivByZero as the name imply an divide by zero error.
 var ErrDivByZero = errors.New("divide by zero")
+
+type Point struct {
+	X, Y int
+}
+
+func round(x float64) int {
+	if x < 0 {
+		return int(math.Ceil(x - 0.5))
+	}
+	return int(math.Floor(x + 0.5))
+}
+
+func Center(x, y, width, height int) Point {
+	x = round(float64((x + x + width)) * 0.5)
+	y = round(float64((y + y + height)) * 0.5)
+	return Point{x, y}
+}
 
 // Ratio return the ratio between x and y. If the value is above 1 then x is
 // greater than y and below 0 y is greater than x.
@@ -14,3 +41,22 @@ func Ratio(x, y int) (float32, error) {
 
 	return float32(x) / float32(y), nil
 }
+
+// Direction checks the ratio to detect the drones possition relative to the
+// object.
+func Direction(ratio float32) (direction int, badness float32) {
+	move := 1 - ratio
+	badness = float32(math.Abs(float64(move)))
+	if 1.0 < badness {
+		badness = 1.0
+	}
+	if -0.05 < move && move < 0.05 {
+		return STAY, 0.0
+	} else if move < -0.05 {
+		return VIRTICAL, badness
+	} else {
+		return HORIZONTAL, badness
+	}
+}
+
+func Placement() {}
--- navigate_test.go
+++ navigate_test.go
@@ -1,8 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
+
+func TestCenter(t *testing.T) {
+	tests := []struct {
+		x, y, width, height int
+		want                Point
+	}{
+		{700, 1412, 843, 823, Point{1122, 1824}},
+		{383, 1044, 1353, 1331, Point{1060, 1710}},
+		{1137, 1277, 825, 977, Point{1550, 1766}},
+		{1096, 1322, 1156, 1157, Point{1674, 1901}},
+		{799, 1871, 549, 570, Point{1074, 2156}},
+	}
+
+	for _, test := range tests {
+		got := Center(test.x, test.y, test.width, test.height)
+		if got != test.want {
+			t.Errorf("Center failed: got: %v, test.want: %v", got, test.want)
+		}
+	}
+
+}
 
 func TestRatio(t *testing.T) {
 	tests := []struct {
@@ -20,5 +42,30 @@ func TestRatio(t *testing.T) {
 		if got != test.want {
 			t.Errorf("got: %v, want: %v", got, test.want)
 		}
+	}
+}
+
+func TestDirection(t *testing.T) {
+	tests := []struct {
+		ratio         float32
+		wantDirection int
+		wantBadness   float32
+	}{
+		{1920.0 / 1080.0, VIRTICAL, 0.777777777},
+		{1080.0 / 1920.0, HORIZONTAL, 0.4375},
+		{1920.0 / 1840.0, STAY, 0.0},
+		{1920.0 / 2000.0, STAY, 0.0},
+	}
+
+	for i, test := range tests {
+		t.Run(fmt.Sprintf("test:%v", i), func(t *testing.T) {
+			d, b := Direction(test.ratio)
+			if d != test.wantDirection {
+				t.Fatalf("Direciton failed: got: %v, want: %v", d, test.wantDirection)
+			}
+			if b != test.wantBadness {
+				t.Errorf("Badness failed: got: %v, want: %v", b, test.wantBadness)
+			}
+		})
 	}
 }
