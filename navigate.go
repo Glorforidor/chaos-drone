524efcfe7f794fde4bd22d66436a82b00b0ea339
Peter Bom Jakobsen <pejak1990@gmail.com>
2017-04-27 22:57:53 +0200

    added Placement and fixed Directions badness.
    all float32 have been made float64 instead.

--- navigate.go
+++ navigate.go
@@ -6,9 +6,17 @@ import (
 )
 
 const (
-	VIRTICAL   = iota
-	HORIZONTAL = iota
-	STAY       = iota
+	VIRTICAL = iota
+	HORIZONTAL
+	STAY
+)
+
+const (
+	NORTH = iota
+	WEST
+	SOUTH
+	EAST
+	ON_TARGET
 )
 
 // ErrDivByZero as the name imply an divide by zero error.
@@ -18,6 +26,7 @@ type Point struct {
 	X, Y int
 }
 
+// round rounds to nearest integer.
 func round(x float64) int {
 	if x < 0 {
 		return int(math.Ceil(x - 0.5))
@@ -25,6 +34,7 @@ func round(x float64) int {
 	return int(math.Floor(x + 0.5))
 }
 
+// Center calculates the center rectangle.
 func Center(x, y, width, height int) Point {
 	x = round(float64((x + x + width)) * 0.5)
 	y = round(float64((y + y + height)) * 0.5)
@@ -34,29 +44,48 @@ func Center(x, y, width, height int) Point {
 // Ratio return the ratio between x and y. If the value is above 1 then x is
 // greater than y and below 0 y is greater than x.
 // An error of type ErrDivByZero is returned if y is zero or less.
-func Ratio(x, y int) (float32, error) {
+func Ratio(x, y int) (float64, error) {
 	if y <= 0 {
 		return 0.0, ErrDivByZero
 	}
 
-	return float32(x) / float32(y), nil
+	return float64(x) / float64(y), nil
 }
 
 // Direction checks the ratio to detect the drones possition relative to the
 // object.
-func Direction(ratio float32) (direction int, badness float32) {
+func Direction(ratio float64) (direction int, badness float64) {
 	move := 1 - ratio
-	badness = float32(math.Abs(float64(move)))
+	direction = STAY
+	badness = math.Abs(math.Log10(ratio))
 	if 1.0 < badness {
 		badness = 1.0
 	}
 	if -0.05 < move && move < 0.05 {
-		return STAY, 0.0
+		badness = 0.0
 	} else if move < -0.05 {
-		return VIRTICAL, badness
+		direction = VIRTICAL
 	} else {
-		return HORIZONTAL, badness
+		direction = HORIZONTAL
 	}
+
+	return direction, badness
 }
 
-func Placement() {}
+// Placement calculates the position of the drone relative to the center of an
+// ring.
+func Placement(x, y, rx, ry int) int {
+	placement := ON_TARGET
+	switch {
+	case x < rx:
+		placement = WEST
+	case x > rx:
+		placement = EAST
+	case y < ry:
+		placement = NORTH
+	case y > ry:
+		placement = SOUTH
+	}
+
+	return placement
+}
